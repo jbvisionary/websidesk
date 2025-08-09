a529458ee3ef1029ffbe8d61f0fe258e650b8825
jbvisionary <tutimax100@gmail.com>
2025-08-10 01:04:53 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-document.getElementById('homeLink').addEventListener('click', (e)=>{
-  e.preventDefault();
-  window.scrollTo({top:0,behavior:'smooth'});
-});
-document.getElementById('exploreBtn').addEventListener('click', ()=>{
-  document.getElementById('products').scrollIntoView({behavior:'smooth'});
-});
